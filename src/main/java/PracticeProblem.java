6f6c6d99ea8c60e8ad14bd6548cf3db55fb61de3
Radin-Ajorlou-ICS4U-02-25 <radin.ajorlou@student.tdsb.on.ca>
2025-03-24 14:18:00 +0000

    done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,6 +4,46 @@ public class PracticeProblem {
 
 	}
 
+
+	public static boolean validIndex(int[] array, int index){
+		try {
+			int b = array[index];
+			return true;
+		}
+		catch(ArrayIndexOutOfBoundsException e){
+			return false;
+			
+		}
+	}
+
+
+
+
+	public static int divide(int num1, int num2){
+		try{
+			return num1/num2;
+		}
+		catch(Exception e){
+			return 0;
+		}
+	}
+
+
+
+
+	public static int safeConvertStringtoInt(String line){
+		try{
+			int num  = Integer.parseInt(line);
+			return num;
+		}
+		catch(NumberFormatException e){
+			return 0;
+		}
+		catch(Exception e){
+			return 0;
+		}
+	}
+
 	
 
 }
